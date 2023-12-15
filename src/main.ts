5887fdf51793b2cf1dc3737e99d68640e058e9e6
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-12-15 12:44:35 +0800

    add base openai call

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import { openai } from './openai';
+
 const form = document.querySelector('#generate-form') as HTMLFormElement;
+const iframe = document.querySelector('#generated-code') as HTMLIFrameElement;
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
@@ -6,5 +9,37 @@ form.addEventListener('submit', async (e) => {
 
   const prompt = formData.get('prompt') as string;
 
-  alert(prompt);
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: `Tu crées des sites web avec Tailwind.
+Ta tâche est généré du code html avec tailwind en fonction du prompt de l'utilisateur.
+Tu renvoie uniquement du HTML sans aucun text avant ou après.
+Tu renvoie du HTML valide.
+Tu n'ajoutes jamais de syntaxe markdown.`,
+      },
+      { role: 'user', content: prompt },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+
+  const code = chatCompletion.choices[0].message.content;
+
+  if (!code) {
+    alert('Erreur: Aucun code généré');
+    return;
+  }
+
+  iframe.srcdoc = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Generated Code</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body>
+    ${code}
+  </body>
+</html>`;
 });
---
+++ src/openai.ts
@@ -0,0 +1,6 @@
+import OpenAI from 'openai';
+
+export const openai = new OpenAI({
+  apiKey: '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',
+  dangerouslyAllowBrowser: true,
+});
